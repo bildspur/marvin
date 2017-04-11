1118aa5571570fb7a777b8126b4e29629fe29976
Florian Bruggisser <florian@nexpose.ch>
2017-04-11 14:06:51 +0200

    added player logic for jumping

---
+++ Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallLogic : MonoBehaviour, IJump
+{
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown ("space"))
+			JumpOnce ();
+	}
+
+	public void JumpOnce ()
+	{
+		rb.AddForce (Vector3.up * 300);
+	}
+
+	public void Jump ()
+	{
+		rb.AddForce (Vector3.up * 500);
+	}
+}
