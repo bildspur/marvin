afaad0eea5c5da4f5a1cc72ddfba34001af5b4b3
Florian Bruggisser <florian@nexpose.ch>
2017-04-11 15:20:08 +0200

    implemented rotating planet

---
+++ Assets/Scripts/PlanetLogic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetLogic : MonoBehaviour {
+	public float speed = 1;
+
+	GameObject planet;
+
+	// Use this for initialization
+	void Start () {
+		planet = this.gameObject;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		planet.transform.Rotate (0, 0, speed);
+	}
+}
