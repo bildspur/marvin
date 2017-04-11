e5f0c5ad7603c3d5bd984477b21a47edf99496b9
Florian Bruggisser <florian@nexpose.ch>
2017-04-11 20:57:51 +0200

    implemented player animation

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -5,28 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class PlayerLogic : MonoBehaviour, IJump
 {
-	Rigidbody2D rb;
+    Rigidbody2D rb;
+    Animator animator;
 
-	// Use this for initialization
-	void Start ()
-	{
-		rb = GetComponent<Rigidbody2D> ();
-	}
 
-	// Update is called once per frame
-	void Update ()
-	{
-		if (Input.GetKeyDown ("space"))
-			JumpOnce ();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
 
-	public void JumpOnce ()
-	{
-		rb.AddForce (transform.up * 300);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+            JumpOnce();
+    }
 
-	public void Jump ()
-	{
-		rb.AddForce (transform.up * 500);
-	}
+    public void JumpOnce()
+    {
+        animator.SetBool("Jumping", true);
+        rb.AddForce(transform.up * 300);
+    }
+
+    public void Jump()
+    {
+        rb.AddForce(transform.up * 500);
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Planet"))
+		{
+            animator.SetBool("Jumping", false);
+        }
+    }
 }
