ac0cfd437c0b0d6012312c924a2b7bf19870dbc1
Florian Bruggisser <florian@nexpose.ch>
2017-04-11 23:31:30 +0200

    added enemy spawn and scoring game

---
+++ Assets/Scripts/EnemyLogic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLogic : MonoBehaviour
+{
+
+    Rigidbody2D rigid;
+    GameObject planet;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        planet = GameObject.FindGameObjectWithTag("Planet");
+
+        // set initial force
+        rigid.AddForce(Vector2.up * 150, ForceMode2D.Force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// look at 2d
+		LookAt2D(planet.transform);
+    }
+
+	public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("EnemyKiller"))
+		{
+            Destroy(this.gameObject);
+        }
+    }
+
+	void LookAt2D(Transform target)
+	{
+        Quaternion rotation = Quaternion.LookRotation
+             (target.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWaitMin;
+    public float spawnWaitMax;
+    public float startWait;
+    public float waveWait;
+
+    public GUIText scoreText;
+    private int score;
+
+    private int wave = 1;
+
+    void Start()
+    {
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(spawnValues.x, spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(Random.Range(spawnWaitMin, spawnWaitMax));
+                AddScore(1);
+            }
+            yield return new WaitForSeconds(waveWait);
+            AddWave();
+        }
+    }
+
+    public void AddWave()
+    {
+        wave ++;
+        UpdateScore();
+    }
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Wave: " + wave + " Score: " + score;
+    }
+
+    void Update()
+    {
+    }
+}
